690bb4b8730f97561c07a44bfffd2895a6083542
LAPTOP-QDS-EI01\VICTOR ORELLANA <victor.orellana@qds.com.pe>
2026-02-16 17:55:15 -0500

    refactor: Replace Hello World placeholder with dashboard page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
+import Page from "@/app/dashboard/page"
+
 export function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="font-medium">Hello World</div>
-    </div>
-  )
+  return <Page />
 }
 
 export default App
