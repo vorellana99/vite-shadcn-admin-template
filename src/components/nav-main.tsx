3560f3f71dea7288943de0db11ec32b26b2a7763
LAPTOP-QDS-EI01\VICTOR ORELLANA <victor.orellana@qds.com.pe>
2026-02-17 00:14:55 -0500

    style: Add collapsible menu animation to sidebar for smoother transitions

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -46,10 +46,10 @@ export function NavMain({
                 <SidebarMenuButton tooltip={item.title}>
                   {item.icon && <item.icon />}
                   <span>{item.title}</span>
-                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                  <ChevronRight className="ml-auto transition-transform duration-500 group-data-[state=open]/collapsible:rotate-90" />
                 </SidebarMenuButton>
               </CollapsibleTrigger>
-              <CollapsibleContent>
+              <CollapsibleContent className="collapsible-menu-animation">
                 <SidebarMenuSub>
                   {item.items?.map((subItem) => (
                     <SidebarMenuSubItem key={subItem.title}>
