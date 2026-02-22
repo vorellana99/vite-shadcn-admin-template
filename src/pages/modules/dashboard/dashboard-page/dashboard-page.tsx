debbeaf5132a209c53593b59a8e4127a160bd536
LAPTOP-QDS-EI01\VICTOR ORELLANA <victor.orellana@qds.com.pe>
2026-02-21 21:12:10 -0500

    feat: update table name of dashsboard table

--- src/pages/modules/dashboard/dashboard-page/dashboard-page.tsx
+++ src/pages/modules/dashboard/dashboard-page/dashboard-page.tsx
@@ -1,5 +1,5 @@
 import { ChartAreaInteractive } from "@/pages/modules/dashboard/dashboard-page/components/chart-area-interactive"
-import { DataTable } from "@/shared/components/data-table/data-table"
+import { SectionsTable } from "@/pages/modules/dashboard/dashboard-page/components/sections-table"
 import { SectionCards } from "@/pages/modules/dashboard/dashboard-page/components/section-cards"
 import data from "./data.json"
 
@@ -8,7 +8,7 @@ export default function DashboardPage() {
     <>
       <SectionCards />
       <ChartAreaInteractive />
-      <DataTable data={data} />
+      <SectionsTable data={data} />
     </>
   )
 }
