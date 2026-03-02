a7c64bec9028c701e6c22f9c197965510a741ecb
LAPTOP-QDS-EI01\VICTOR ORELLANA <victor.orellana@qds.com.pe>
2026-03-01 19:21:51 -0500

    style: set border to solid

--- src/shared/components/data-table/data-table-view.tsx
+++ src/shared/components/data-table/data-table-view.tsx
@@ -18,7 +18,7 @@ export function DataTableView<TData>({
     columnsLength,
 }: DataTableViewProps<TData>) {
     return (
-        <div className="overflow-hidden rounded-lg border">
+        <div className="overflow-hidden rounded-lg border border-black/15 [&_tr]:border-black/15">
             <Table>
                 <TableHeader className="bg-primary/90 [&_button:hover]:bg-white/10 sticky top-0 z-10 [&_th]:text-primary-foreground [&_svg]:text-primary-foreground [&_button]:text-primary-foreground hover:[&_button]:text-primary-foreground">
                     {table.getHeaderGroups().map((headerGroup) => (
@@ -42,7 +42,7 @@ export function DataTableView<TData>({
                             <TableRow
                                 key={row.id}
                                 data-state={row.getIsSelected() && "selected"}
-                                className="relative even:bg-muted/60 hover:bg-primary/20"
+                                className="relative even:bg-primary/5 hover:bg-primary/20"
                             >
                                 {row.getVisibleCells().map((cell) => (
                                     <TableCell key={cell.id}>
